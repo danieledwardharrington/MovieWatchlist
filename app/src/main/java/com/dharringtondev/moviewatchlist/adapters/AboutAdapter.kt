188e018eff250c8a4abb82cb912eded53ebe492a
Daniel Harrington <40922466+danieledwardharrington@users.noreply.github.com>
2020-12-24 18:39:07 -0700

    more about stuff

--- app/src/main/java/com/dharringtondev/moviewatchlist/adapters/AboutAdapter.kt
+++ app/src/main/java/com/dharringtondev/moviewatchlist/adapters/AboutAdapter.kt
@@ -1,9 +1,7 @@
 package com.dharringtondev.moviewatchlist.adapters
 
-import android.graphics.drawable.Drawable
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.dharringtondev.moviewatchlist.R
@@ -13,8 +11,8 @@ class AboutAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val TAG = "AboutAdapter"
 
-    private val aboutList = arrayListOf<String>("Donate", "Rate", "Share", "Licenses")
-    private val iconList = arrayListOf<Int>(R.drawable.ic_donate, R.drawable.ic_rate, R.drawable.ic_share, R.drawable.ic_licenses)
+    private val aboutList = arrayListOf<String>("Donate", "Rate", "Share", "Libraries")
+    private val iconList = arrayListOf<Int>(R.drawable.ic_donate, R.drawable.ic_rate, R.drawable.ic_share, R.drawable.ic_libraries)
     private lateinit var clickedListener: OnItemClickedListener
 
     interface OnItemClickedListener {
